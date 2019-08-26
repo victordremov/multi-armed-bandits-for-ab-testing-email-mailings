a95e9dfab65665ef232956a4eca27f4c7ca6fe9f
victordremov <32140716+victordremov@users.noreply.github.com>
2019-08-26 18:20:28 +0300

    Update urls.py

--- bandits_site/urls.py
+++ bandits_site/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', include(bandits_site.bandits_site.urls)),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
