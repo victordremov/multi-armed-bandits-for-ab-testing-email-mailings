95529cb44116d76eda6a052530aeb33300650bba
victordremov <32140716+victordremov@users.noreply.github.com>
2019-08-26 17:42:20 +0300

    Update settings.py

--- bandits_site/settings.py
+++ bandits_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@%h+fyq_q)c99#*(p=a_13o68$^qje=q7h^(-xttpm4iwk180^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['victordremov.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
